81a2c0b1b6805a036d41ea1aa20f3b77140f23f7
Aadil Tamboli <aadiltamboli07@gmail.com>
2026-04-12 16:38:15 +0530

    backend setup done and connection with atlas DB

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Signup from './pages/Auth/Signup'
+import Login from './pages/Auth/Login'
 
 const App = () => {
   return (
     <div className='text-red-400'>
-      App
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login'element={<Login/>} />
+       <Route path='/sign-up' element={<Signup/>}/>
+      </Routes>
     </div>
   )
 }
