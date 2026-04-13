ede75fd2473a8b86b307b6ea4d2b6ea929782d14
Aadil Tamboli <aadiltamboli07@gmail.com>
2026-04-13 13:18:39 +0530

    added image-upload route

--- backend/Controllers/story.controller.js
+++ backend/Controllers/story.controller.js
@@ -1,4 +1,5 @@
 import Story from "../Models/story.model.js";
+import cloudinary from "../Lib/cloudinary.js";
 export const addStory = async (req, res) => {
     try {
         const { title, story, visitedDate, imageURL, visitedLocation } = req.body;
@@ -31,3 +32,39 @@ export const getAllStory = async (req, res) => {
         console.log("Error Occure in the get all story controller ")
     }
 }
+
+export const imageUpload = async (req, res) => {
+    try {
+        // checking for file is uploaded or not 
+        if (!req.file) {
+            return res.status(400).json({ message: "No file uploaded" });
+        }
+
+        //  upload to cloudinary
+        const stream = cloudinary.uploader.upload_stream(
+            { folder: "myapp" }, // 
+            (error, result) => {
+                if (error) {
+                    return res.status(500).json({ error: error.message });
+                }
+
+                return res.status(200).json({
+                    message: "Uploaded successfully",
+                    ImageURL: result.secure_url,
+                });
+            }
+        );
+
+        //  send buffer to cloudinary
+        stream.end(req.file.buffer);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+
+
+
+
+module.exports = router;
--- backend/routes/story.route.js
+++ backend/routes/story.route.js
@@ -1,11 +1,14 @@
 import express from "express";
 
 import { verifyUser } from "../Lib/verifyUser.js";
-import { addStory, getAllStory } from "../Controllers/story.controller.js";
+
+import { addStory, getAllStory, imageUpload } from "../Controllers/story.controller.js";
+import upload from "../Lib/multer.js";
 const router = express.Router();
 
 
 router.post("/story", verifyUser, addStory);
+router.post("/image", upload.single("image"), imageUpload);
 router.get("/allstory", verifyUser, getAllStory);
 
 export default router;
