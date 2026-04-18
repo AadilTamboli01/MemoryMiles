b4c42934d43c3d1fb5d919df9ea17dbd055e86c7
Aadil Tamboli <aadiltamboli07@gmail.com>
2026-04-19 00:46:43 +0530

    fixes .env file

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,8 @@ app.use("/api/user", userRouter);
 app.get("/", (req, res) => {
     res.send("request on root")
 })
-app.listen(3000, () => {
+const PORT = process.env.PORT||3000;
+app.listen(PORT, () => {
     console.log("App is listening of port 3000")
 })
 
