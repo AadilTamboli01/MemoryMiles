6278430f2e08415c1003363dbc02d228d3ec5ca6
Aadil Tamboli <aadiltamboli07@gmail.com>
2026-04-19 14:41:40 +0530

    fixes the multer isssue

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 // https://memorymiles-uzac.onrender.com
-const BASE_URL = "https://memorymiles-backend.onrender.com/api";
+// const BASE_URL = "https://memorymiles-backend.onrender.com/api";
+const BASE_URL = "https://127.0.0.1:3000/api"
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
