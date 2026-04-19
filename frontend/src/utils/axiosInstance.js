91218e435dfbf126634752346f1302a0ff70b18a
Aadil Tamboli <aadiltamboli07@gmail.com>
2026-04-19 14:54:19 +0530

    added new backend url

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 // https://memorymiles-uzac.onrender.com
 // const BASE_URL = "https://memorymiles-backend.onrender.com/api";
-const BASE_URL = "https://127.0.0.1:3000/api"
+const BASE_URL = "https://memorymiles-backend-y80z.onrender.com/api"
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
